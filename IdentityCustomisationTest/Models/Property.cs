9fdd59f51e1d66e2c42502359f4cfb67369b6854
Jon Jackson <jon@acuras.co.uk>
2018-12-11 12:12:23 +0000

    Rename Properties Class to Property (to avoid namespace conflict)

--- IdentityCustomisationTest/Models/Property.cs
+++ IdentityCustomisationTest/Models/Property.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IdentityCustomisationTest.Models
 {
-    public class Properties
+    public class Property
     {
         public int ID { get; set; }
         public string HouseNum { get; set; }
