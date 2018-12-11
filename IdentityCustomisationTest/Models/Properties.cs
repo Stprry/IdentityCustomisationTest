984b52c334da0dcff00871fb7d8c26fe5207decf
Jon Jackson <jon@acuras.co.uk>
2018-12-11 12:07:56 +0000

    Added Properties and Room models

---
+++ IdentityCustomisationTest/Models/Properties.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityCustomisationTest.Models
+{
+    public class Properties
+    {
+        public int ID { get; set; }
+        public string HouseNum { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        [MaxLength(10)]
+        public string PostCode { get; set; }
+        public string MainImage { get; set; }
+
+        public ICollection<Room> Rooms { get; set; }
+
+    }
+}
---
+++ IdentityCustomisationTest/Models/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityCustomisationTest.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string ThreeSixtyImage { get; set; }
+
+        /*
+         * Should really setup a Floor class and link Rooms to Floors
+         * Properties will then be linked to Rooms via Floors
+         * */
+    }
+}
